bd36dacad47730b2e71ace0578ea50b500dd473f
sorokovikov_as <artghostcrayne@mail.ru>
2022-10-05 13:47:56 +0300

    Modif code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,110 +1,70 @@
 import java.time.LocalDateTime
 
 data class Message(
-    val messageId: Int,
-    val receiverId: Int,
-    val senderId: Int,
-    val chatId: Int,
+    var sender: Int,
     var date: LocalDateTime = LocalDateTime.now(),
     val text: String,
-    var status: Int = 0
+    var read: Boolean = false
 )
 
-data class Chat(val chatId: Int, val receiverId: Int, val senderId: Int, var unRead: Boolean = true)
+data class Chat(var messages: MutableList<Message> = mutableListOf())
 
 object Chats {
-    private var lastChatId = 0
-    private var lastMessageId = 0
-    var messages: MutableList<Message> = arrayListOf()
-    private var chats: MutableList<Chat> = arrayListOf()
+    var chats: MutableMap<Pair<Int,Int>,Chat> = mutableMapOf()
 
-    fun addChat(receiverId: Int, senderId: Int): Int {
-        chats.add(Chat(++lastChatId, receiverId, senderId))
-        return lastChatId
+    fun addMessage(receiverId: Int, senderId: Int,  text: String) {
+        chats.getOrPut(receiverId toSorted senderId){Chat()}.messages.add(Message(sender = senderId,text = text))
     }
 
-    fun checkChat(receiverId: Int, senderId: Int): Int {
-        return when (chats.none { it.receiverId == receiverId && it.senderId == senderId }) {
-            true -> 0
-            false -> chats.last { it.receiverId == receiverId && it.senderId == senderId }.chatId
-        }
+    fun readChat(receiverId: Int, senderId: Int, count: Int): List<Message>{
+        val chat = chats[receiverId toSorted senderId]
+        return chat?.messages?.takeLast(count).orEmpty().onEach { it.read = true }
     }
 
-    fun addMessage(receiverId: Int, senderId: Int, chatId: Int, text: String): Boolean {
-        chats.last { it.chatId == chatId }.chatId
-        return messages.add(Message(++lastMessageId, receiverId, senderId, chatId, text = text))
+    fun delChat(receiverId: Int, senderId: Int){
+        chats[receiverId toSorted senderId]?.messages?.clear()
+        chats.remove(receiverId toSorted senderId)
     }
 
-    fun newMessage(receiverId: Int, senderId: Int, text: String) {
-        var id = checkChat(receiverId, senderId)
-
-        if (id == 0) {
-            id = addChat(receiverId, senderId)
-        }
-        addMessage(receiverId, senderId, id, text)
+    fun delMessage(receiverId: Int, senderId: Int, index: Int){
+        chats[receiverId toSorted senderId]?.messages?.removeAt(index)
+        if (chats[receiverId toSorted senderId]?.messages?.size == 0) chats.remove(receiverId toSorted senderId)
     }
 
-    fun getChatsByUser(userId: Int): List<Chat> {
-        return chats.filter { it.receiverId == userId }
-    }
-
-    fun getUnreadChatsCount(receiverId: Int): Int {
-        return chats.filter { it.receiverId == receiverId && it.unRead }.size
-    }
-
-    fun delChat(id: Int) {
-//        chats = chats.filter { it.chatId != id }.toMutableList()
-        chats.dropWhile { it.chatId == id }
-        messages = messages.dropWhile { it.chatId == id }.toMutableList()
-    }
-
-    fun delMessage(messageId: Int) {
-        val chatId = messages.last { it.messageId == messageId }.chatId
-        messages = messages.dropWhile { it.messageId == messageId }.toMutableList()
-        var mes = messages.filter { it.messageId == messageId }
-        if (mes.isEmpty()) {
-            delChat(chatId)
-        }
+    fun getUnreadChatsCount(): Int {
+        return chats.values.count{it.messages.filter { !it.read }.isNotEmpty()}
     }
 
     fun getChats() {
         chats.forEach {
-            val mes = messages.last { message: Message -> message.chatId == it.chatId }.text
-            if (mes.isEmpty()) println("No messages")
-            else println(mes)
+            print(it.value.messages.last())
         }
     }
 
-    fun readChats(chatId: Int, messageId: Int, count: Int) {
-        val mes = messages.filter { it.messageId >= messageId && it.chatId == chatId }
-        val cnt = 0
-        mes.forEach {
-            messages[messages.indexOfLast { message: Message -> message.messageId == it.messageId }].status = 1
-            chats[chats.indexOfLast { chat: Chat -> chat.chatId == it.chatId }].unRead = false
-            println("${it.date}: ${it.text}")
-        }
-    }
 }
 
+private infix fun Int.toSorted(senderId: Int): Pair<Int, Int> = if (this<senderId) Pair(senderId,this) else Pair(this,senderId)
 
 fun main(args: Array<String>) {
 
     val service = Chats
-    service.newMessage(999, 1, "chat 1 mes 1")
-    service.newMessage(999, 3, "chat 3 mes 1")
-    service.newMessage(999, 2, "chat 2 mes 1")
-    service.newMessage(999, 1, "chat 1 mes 2")
-    service.newMessage(999, 2, "chat 2 mes 2")
-    service.newMessage(999, 2, "chat 2 mes 3")
-    service.newMessage(999, 1, "chat 1 mes 4")
-    service.newMessage(999, 1, "chat 1 mes 5")
-    service.newMessage(2, 1, "chat 4 mes 1")
+    service.addMessage(999, 1, "Hi")
+    service.addMessage(999, 2, "Hi")
+    service.addMessage(1, 999, "Hi")
+    service.addMessage(999, 1, "How are you")
+    service.addMessage(1, 999, "I am fine")
+    service.addMessage(999, 1, "Me too")
+    println(service.getUnreadChatsCount())
+    println(service.readChat(999,1,30))
+    println(service.getUnreadChatsCount())
 
-    println(service.getChatsByUser(2))
+//    service.delChat(999,1)
+    println(service.chats)
 
-//    println(service.getChats())
+//    service.delMessage(999,2,0)
+    println(service.chats)
 
-    service.readChats(1, 5, 5)
+    service.getChats()
 
 
 }
